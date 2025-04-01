9cf4ed7a27a296fce847f067530a9ce13ce43fd5
Ursule <aizannonursule@gmail.com>
2025-04-01 21:30:49 +0100

    responsive

---
+++ styles/responsive.js
@@ -0,0 +1,7 @@
+const berger=document.querySelector('.fa-bars');
+const menu=document.querySelector('.nav_bloc2');
+
+ berger.addEventListener ('click' , function() 
+  {
+    menu.classList.toggle('active');
+});
